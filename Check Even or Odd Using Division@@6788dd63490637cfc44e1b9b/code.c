1d20a3fa428a41df749c5891650b3f2fad92ba25
kingerharshit <kingerharshit07@gmail.com>
2025-02-15 11:08:18 +0530

    Solution to Check Even or Odd Using Division

--- Check Even or Odd Using Division@@6788dd63490637cfc44e1b9b/code.c
+++ Check Even or Odd Using Division@@6788dd63490637cfc44e1b9b/code.c
@@ -7,7 +7,7 @@ int main() {
     scanf("%d ", &num);
     
     // Check if num is a multiple of divisor
-    if (num != 0 && num % 2 == 0) {
+    if (num = 0 && num % 2 == 0) {
         printf("Even");
     } else {
         printf("Odd");
